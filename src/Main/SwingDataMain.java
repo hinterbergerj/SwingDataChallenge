7e5699f44a4d9f891119911fa4c6743937fc5d70
Jessica Devlin <devlinj1@duq.edu>
2019-03-31 17:58:56 -0400

    starting tracking for project

---
+++ src/Main/SwingDataMain.java
@@ -0,0 +1,31 @@
+/* This program was written
+ * by Jessica Devlin
+ * on 03/29/19
+ * last modified on 03/31/19
+ * for Diamond Kinetics
+ * This is a program written to analyze data given by a sample swing using different methods of scrutiny
+ * This program implements a data structure called SwingDimension which contains the data provided in a sample csv
+ * SwingDimension also contains the completed methods as defined by the coding challenge provided
+ * A Pair class has also been created to return the appropriate information for the last method in SwingDimension
+ */
+
+package Main;
+
+import java.util.ArrayList;
+
+public class SwingDataMain {
+
+	public static void main (String[] args) {
+		
+		SwingDimension swing = new SwingDimension();
+		String filename = "src\\data\\latestSwing.csv";
+		swing.readIn(filename);
+		
+		System.out.println(swing.searchContinuityAboveValue("ax", 0, 20, 3000.0, 5));
+		System.out.println(swing.backSearchContinuityWithinRange("ax", 7, 2, 3000.0, 6000.0, 2));
+		ArrayList<Pair> rslt = swing.searchMultiContinuityWithinRange("ay", 0, 1000, 0, 6, 3);
+		for (int i = 0; i < rslt.size(); i++) {
+			System.out.println(rslt.get(i).toString());
+		}
+	} 
+}	
